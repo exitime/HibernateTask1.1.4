9e97c089955192ed7f2a7b012d0a1d4c133d4563
Max Kuznetsov <kevokk31@gmail.com>
2021-10-10 23:28:00 +0300

    выполнение задания 1.1.4
    Signed-off-by: Max Kuznetsov <kevokk31@gmail.com>

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,119 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
+import javax.persistence.PersistenceException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
 
     }
+    private static final SessionFactory sessionFactory = Util.getSessionFactory();
 
-
-    @Override
-    public void createUsersTable() {
-
+    private void SQLQuery(String SQL) {
+        Transaction t = null;
+        try (Session s = Util.getSession()) {
+            t = s.beginTransaction();
+            s.createSQLQuery(SQL)
+                    .executeUpdate();
+            t.commit();
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+            if (t != null) {
+                t.rollback();
+            }
+        }
     }
 
     @Override
-    public void dropUsersTable() {
+    public void createUsersTable() {
+        String SQL = "CREATE TABLE IF NOT EXISTS users " +
+                "(id bigint primary key auto_increment, name varchar(255), lastname varchar(255), age tinyint)";
+        SQLQuery(SQL);
+    }
+//        Session s = sessionFactory.openSession();
+//        Transaction t = s.beginTransaction();
+//        try {
+//            s.createSQLQuery("CREATE TABLE IF NOT EXISTS users " +
+//                    "(id INT PRIMARY KEY AUTO_INCREMENT, " +
+//                    "name VARCHAR(45), " +
+//                    "lastname VARCHAR(45), " +
+//                    "age INT);").executeUpdate();
+//            t.commit();
+//        } catch (HibernateException e) {
+//            t.rollback();
+//        } finally {
+//            s.close();
+//        }
 
+    @Override
+    public void dropUsersTable() {
+        String SQL = "DROP TABLE IF EXISTS users";
+        SQLQuery(SQL);
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        Transaction t = null;
+        try (Session s = Util.getSession()) {
+            t = s.beginTransaction();
+            s.save(new User(name, lastName, age));
+            t.commit();
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+            if (t != null) {
+                t.rollback();
+            }
+        }
+        System.out.println("Пользователь с именем " + name + " добавлен");
     }
 
     @Override
     public void removeUserById(long id) {
-
+        Transaction t = null;
+        try (Session s = Util.getSession()) {
+            t = s.beginTransaction();
+            s.createQuery("DELETE FROM User WHERE id = :id")
+                    .setParameter("id", id)
+                    .executeUpdate();
+            t.commit();
+        } catch (PersistenceException e) {
+            e.printStackTrace();
+            if (t != null) {
+                t.rollback();
+            }
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        Transaction t = null;
+        List<User> users = new ArrayList<>();
+        try (Session s = sessionFactory.openSession()) {
+            t = s.beginTransaction();
+            users = s.createQuery("FROM User").getResultList();
+            t.commit();
+        } catch (HibernateException e) {
+            e.printStackTrace();
+            if (t != null) {
+                t.rollback();
+            }
+        }
+        return users;
     }
+
+
 
     @Override
     public void cleanUsersTable() {
-
+        String SQL = "DELETE FROM users";
+        SQLQuery(SQL);
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,12 +1,18 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
     // реализуйте настройку соеденения с БД
-
+    private static final SessionFactory sessionFactory = buildSessionFactory();
     private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
     private static final String USER = "root";
@@ -22,4 +28,39 @@ public class Util {
         }
         return conn;
     }
+
+
+
+    private static SessionFactory buildSessionFactory() {
+
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(User.class);
+        configuration.setProperty("connection.driver_class", DRIVER);
+        configuration.setProperty("hibernate.connection.url", URL);
+        configuration.setProperty("hibernate.connection.username", USER);
+        configuration.setProperty("hibernate.connection.password", PASSWORD);
+        configuration.setProperty("dialect", "org.hibernate.dialect.MySQLDialect");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+        configuration.setProperty("show_sql", "true");
+
+        StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties());
+        try {
+            return configuration.buildSessionFactory(builder.build());
+        }
+        catch (Exception e) {
+            throw new ExceptionInInitializerError("Initial SessionFactory failed" + e);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static Session getSession() {
+        return sessionFactory.openSession();
+    }
+
+
+
 }
